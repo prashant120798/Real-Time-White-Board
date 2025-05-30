2e67ce969b9e62a20c7b7ee6fdcd0a8886e906b7
prashant120798 <prashanttanwar29@gmail.com>
2025-05-30 20:16:40 +0530

    added render

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://pepboard-1.onrender.com");
+const socket = io.connect("https://real-time-white-board-2rmn.onrender.com");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
